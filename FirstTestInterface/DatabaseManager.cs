2b5a05b2a33923456c420ec12b804e740abfcdd3
NavaleAbhay <navale.abhay12@gmail.com>
2023-12-20 07:48:39 +0530

    importing data and creating csv file

---
+++ FirstTestInterface/DatabaseManager.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTestInterface
+{
+    class DatabaseManager
+    {
+        private string connectionString = "server=localhost;port=3306;user=root;password=password;database=TestDB"; 
+
+        public DataTable GetEmployeeData()
+        {
+            DataTable dataTable = new DataTable();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Employee";
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        public void ExportToCSV(DataTable dataTable, string filePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            IEnumerable<string> columnNames = dataTable.Columns.Cast<DataColumn>().
+                                              Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+
+            File.WriteAllText(filePath, sb.ToString());
+        }
+    }
+}
+
---
+++ FirstTestInterface/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstTestInterface
+{
+    public partial class Form1 : Form
+    {
+        //private readonly System.Windows.Forms.DataGridView dataGridView;
+        private DataGridView dataGridView;
+        private DatabaseManager dbManager = new DatabaseManager();
+        private Button btnGetData;
+        private Button btnExportCSV;
+
+        public Form1()
+        {
+            InitializeComponents();
+            //InitializeDataGridView();
+        }
+        private void InitializeComponents()
+        {
+            dataGridView = new DataGridView();
+            dataGridView.Dock = DockStyle.Fill;
+            btnGetData = new Button();
+            btnGetData.Text = "Get Data";
+            btnGetData.Click += btnGetData_Click;
+            btnExportCSV = new Button();
+            btnExportCSV.Text = "Export to CSV";
+            btnExportCSV.Click += btnExportCSV_Click;
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+            tableLayoutPanel.Dock = DockStyle.Top;
+            tableLayoutPanel.AutoSize = true;
+            tableLayoutPanel.Controls.Add(btnGetData);
+            tableLayoutPanel.Controls.Add(btnExportCSV);
+            Controls.Add(dataGridView);
+            Controls.Add(tableLayoutPanel);
+        }
+
+        //private void InitializeDataGridView()
+        //{
+        //    // Create a new DataGridView
+        //    dataGridView = new DataGridView();
+        //    dataGridView.Dock = DockStyle.Fill;
+        //    Controls.Add(dataGridView);
+        //}
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            DataTable employeeData = dbManager.GetEmployeeData();
+            dataGridView.DataSource = employeeData;
+        }
+
+        private void btnExportCSV_Click(object sender, EventArgs e)
+        {
+            DataTable employeeData = (DataTable)dataGridView.DataSource;
+            string fileName = DateTime.Now.ToString("ddMMyyyy") + ".csv";
+            string filePath = Path.Combine(@"D:\AbhayServices\FirstService\FirstTestInterface\bin\Debug", fileName);
+            dbManager.ExportToCSV(employeeData, filePath);
+            MessageBox.Show("CSV file exported successfully!");
+        }
+    }
+}
